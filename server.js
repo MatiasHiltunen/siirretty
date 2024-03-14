e07a9b9926936ee7129d49c26b7c8ea434087d04
Matias Hiltunen <matias.s.j@hotmail.com>
2024-03-14 18:58:03 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,37 @@
+import express, { Router } from "express";
+
+const app = express();
+const router = Router();
+
+router.get('/user', (req, res)=>{
+    res.send('Kaikki käyttäjät haettu, count: 1234, page: 1, limit: 20')
+})
+
+router.get('/user/:id', (req, res)=>{
+    const id = req.params.id
+    res.send("Käyttäjän tiedot id:llä: " + id)
+})
+
+router.post('/user', (req, res)=>{
+    res.status(201).send('Käyttäjä luotu onnistuneesti')
+})
+
+router.put('/user', (req, res)=>{
+    res.send('Käyttäjä päivitetty onnistuneesti')
+})
+
+router.patch('/user', (req, res)=>{
+    res.send('Käyttäjän ikä päivitetty onnistuneesti')
+})
+
+router.delete('/user', (req, res)=>{
+    res.send('Käyttäjätili poistettu onnistuneesti')
+})
+
+app.use('/api/v1', router)
+
+app.use(express.static('public'))
+
+app.listen(3000, ()=>{
+    console.log('HTTP Server is running on port http://localhost:3000')
+})
