8c563a27b46e36b9a79423200955a8f727f95283
Matias Hiltunen <matias.s.j@hotmail.com>
2024-03-14 19:52:42 +0200

    add sqlite3

---
+++ database/sqlite.js
@@ -0,0 +1,17 @@
+import sqlite3 from "sqlite3";
+
+const db = new sqlite3.Database('db.sqlite')
+
+db.exec("CREATE TABLE IF NOT EXISTS user (username TEXT, age INT)")
+
+db.serialize(()=>{
+
+
+    const stmt = db.prepare("INSERT INTO user VALUES (?, ?)")
+
+    stmt.run("Mie", 30)
+
+    stmt.finalize()
+
+})
+
